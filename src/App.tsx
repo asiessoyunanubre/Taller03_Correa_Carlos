2425aab9857ec84144647a232a169d5ab5e96066
Juan David Peña De La Torre <junadavidpena1112@gmail.com>
2024-09-16 09:41:16 -0500

    proyecto react

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "./assets/img/logoReact.png"
+
+function App() {
+
+  return (
+    <>
+   <div>
+    <img src={miLogo} alt="logo de React" />
+   <h1>¡¡¡hola mundo !!!</h1>
+   <p>  Este es mi primer proyectp en react...</p>
+   </div>
+    </>
+  );
+}
+
+export default App;
